a39306be1cd1f44b1ef3f8d0a3d0f11284fac5de
A <A@ANUJ>
2023-09-30 21:20:36 -0400

    Add project files.

---
+++ Ishita 4ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ishita_4ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(test(3));
+            Console.WriteLine(test(14));
+            Console.WriteLine(test(12));
+            Console.WriteLine(test(37));
+            Console.ReadLine();
+        }
+        public static bool test(int a)
+        {
+            return a % 3 == 0 || a % 7 == 0;
+        }
+    }
+}
